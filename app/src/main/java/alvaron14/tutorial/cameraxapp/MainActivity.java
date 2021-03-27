19f1fcc8b1e6e481bb5a6a25049f5c468f298fa7
Álvaro Gómez Cuenca <basura1200@gmail.com>
2021-03-27 13:26:43 +0100

    Initial commit

---
+++ app/src/main/java/alvaron14/tutorial/cameraxapp/MainActivity.java
@@ -0,0 +1,150 @@
+package alvaron14.tutorial.cameraxapp;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.core.impl.ImageCaptureConfig;
+import androidx.camera.core.impl.utils.executor.CameraXExecutors;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import alvaron14.tutorial.cameraxapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageCapture imageCapture;
+    private ActivityMainBinding binding;
+    private int REQUEST_CODE_PERMISSIONS = 101;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA",
+            "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        PreviewView previewView = binding.viewFinder;
+
+        ListenableFuture cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                // Camera provider is now guaranteed to be available
+                ProcessCameraProvider cameraProvider = (ProcessCameraProvider) cameraProviderFuture.get();
+
+                // Set up the view finder use case to display camera preview
+                Preview preview = new Preview.Builder().build();
+
+                // Set up the capture use case to allow users to take photos
+                imageCapture = new ImageCapture.Builder()
+                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                        .build();
+
+                // Choose the camera by requiring a lens facing
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                        .build();
+
+                // Attach use cases to the camera with the same lifecycle owner
+                Camera camera = cameraProvider.bindToLifecycle(
+                        ((LifecycleOwner) this),
+                        cameraSelector,
+                        preview,
+                        imageCapture);
+
+                // Connect the preview use case to the previewView
+                preview.setSurfaceProvider(
+                        previewView.getSurfaceProvider());
+            } catch (InterruptedException | ExecutionException e) {
+                // Currently no exceptions thrown. cameraProviderFuture.get()
+                // shouldn't block since the listener is being called, so no need to
+                // handle InterruptedException.
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+        binding.cameraCaptureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.onClick();
+            }
+        });
+
+        /*
+        // The use case is bound to an Android Lifecycle with the following code
+        Camera camera = cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview);
+
+        // PreviewView creates a surface provider, using a Surface from a different
+        // kind of view will require you to implement your own surface provider.
+        preview.previewSurfaceProvider = viewFinder.getSurfaceProvider();
+         */
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(allPermissionsGranted()){
+            //startCamera();
+        } else{
+            Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    private boolean allPermissionsGranted(){
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(getApplicationContext(), permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void onClick() {
+        File outputFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                "test_" + System.currentTimeMillis() + ".png");
+
+        try {
+            outputFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(outputFile).build();
+        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this), //Executors.newSingleThreadExecutor()
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                        Log.d("PHOTO", "onImageSaved: saved");
+                    }
+
+                    @Override
+                    public void onError(ImageCaptureException error) {
+                        Log.d("PHOTO", "onError: " + error);
+                    }
+        });
+    }
+}
